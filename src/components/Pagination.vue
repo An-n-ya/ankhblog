b319aae3a63c65dbe82b2328a78dff0de61ba3d1
ankh <ankh04@icloud.com>
2021-08-16 09:12:08 +0800

    fixed

---
+++ src/components/Pagination.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center">
+    <v-pagination v-model="page" :length="6"></v-pagination>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
